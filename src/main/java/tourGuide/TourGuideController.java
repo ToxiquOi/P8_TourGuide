9425e68558b50360a68790e3317bacf2d3a1f632
Tox- <gau.vui97@hotmail.com>
2023-04-20 12:21:58 +0200

    Fix getNearByAttraction, Add Swagger

--- src/main/java/tourGuide/TourGuideController.java
+++ src/main/java/tourGuide/TourGuideController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import gpsUtil.location.Location;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,12 +42,12 @@ public class TourGuideController {
     }
 
     @GetMapping("/getNearbyAttractions")
-    public String getNearbyAttractions(@RequestParam String userName) {
+    public ResponseEntity<AttractionProximityModel> getNearbyAttractions(@RequestParam String userName) {
         User user = getUser(userName);
     	VisitedLocation visitedLocation = tourGuideService.getUserLocation(user);
         AttractionProximityModel attractionProximityModel = tourGuideService.getNearByAttractions(visitedLocation);
         attractionProximityModel.setUser(user);
-    	return JsonStream.serialize(attractionProximityModel);
+    	return ResponseEntity.ok(attractionProximityModel);
     }
     
     @GetMapping("/getRewards")
